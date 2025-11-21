25952d04fc89d5494c1f6c8de53e5e44473e2b1b
조수민 <chosm0129@naver.com>
2025-11-21 12:33:16 +0900

    배열 데이터 API 테스트 - 유저 추가

---
+++ controller/auth.mjs
@@ -0,0 +1,36 @@
+import express from "express";
+import * as authRepository from "../data/auth.mjs";
+
+// export async function signup(req, res, next) {
+//     const { userid, password, name, email } = req.body;
+//     const user = await authRepository.signup(userid, password, name, email);
+//     res.status(201).json(user);
+// }
+
+// export async function login(req, res, next) {
+//     const { userid, password } = req.body;
+//     const user = await authRepository.login(userid, password);
+//     if(user) {
+//         res.status(200).json({ message: '로그인 성공!' });
+//     } else {
+//         res.status(404).json({ message: '올바르지 않은 아이디 또는 비밀번호 입니다.' });
+//     }
+// }
+
+export async function signup(req, res, next) {
+    const { userid, password, name, email } = req.body;
+    const user = await authRepository.createUser(userid, password, name, email);
+    if(user) {
+        res.status(201).json(user);
+    }
+}
+
+export async function login(req, res, next) {
+    const { userid, password } = req.body;
+    const user = await authRepository.login(userid, password);
+    if(user) {
+        res.status(200).json({ message: `${userid}님 로그인 완료!` });
+    } else {
+        res.status(200).json({ message: `${userid}님 아이디 또는 비밀번호를 확인하세요.` });
+    }
+}
--- router/auth.mjs
+++ router/auth.mjs
@@ -1,10 +1,15 @@
 import express from "express";
+import * as authController from "../controller/auth.mjs"
 
 const router = express.Router();
 
 // 회원가입
+// 127.0.0.1/auth/signup
+router.post("/signup", authController.signup);
 
 // 로그인
+// 127.0.0.1/auth/login
+router.post("/login", authController.login);
 
 // 로그인 유지
 
